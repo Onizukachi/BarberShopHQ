ea6f7cdbb2aa9f79bc2857c34d179f977cdaf52d
onizuka <dark_sao@mail.ru>
2021-10-30 14:49:17 +0300

    Add migration

--- app.rb
+++ app.rb
@@ -1,8 +1,36 @@
-#encoding: utf-8
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require "sinatra/activerecord"
+
+set :database, {adapter: "sqlite3", database: "barbershop.db"}
+
+#Код ниже отвечает за настройку activerecord для нашей сущности
+class Client < ActiveRecord::Base
+end
+
+class Barber < ActiveRecord::Base	
+end
+
+before do
+	@barbers = Barber.all
+end
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb :index	
+end
+
+get '/visit' do
+    #Присваиваем таблицу Barbers для вывода, сортируя по id
+    erb :visit
+end
+
+post '/visit'do
+    @username = params[:username]
+    @phone = params[:phone]
+    @date_name = params[:date_name]
+    @barba = params[:barber]
+    @color = params[:color]
+
+    erb "<h2>Спасибо, вы записались</h2>"
 end
